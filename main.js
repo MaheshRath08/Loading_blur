bd652e0d7c8ce49ac130b887276c4bbf297ff910
MaheshRath08 <maheshrath.2002@gmail.com>
2022-12-03 00:09:43 +0530

    finl commit

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 let bg = document.querySelector(".bg")
-let status = document.querySelector(".status")
+let statusEl = document.querySelector(".status")
+let loading = 0
+
+
+let run = setInterval(process, 30)
+function process(){
+    loading++
+
+    statusEl.textContent = `${loading}%`
+    if(loading>=100){
+        clearInterval(run)
+    }
+
+    bg.style.filter = `blur(${scale(loading, 0, 100, 20, 0)}px) 
+                    saturate(${(scale(loading, 0,100,0,100))}%)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
